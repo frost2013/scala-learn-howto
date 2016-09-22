cef82b829d57f576ef40ceabe92dff4baa7ee46f
nebiroz <kovalski2006@yandex.ru>
2016-09-22 18:40:36 +0300

    Сделал список шагов: 6 - циклы и переборы 7 - использование массивов 8 - использование списков 9 - начал делать

---
+++ src/com/nebiroz/scala/Lesson1.scala
@@ -0,0 +1,12 @@
+package com.nebiroz.scala
+
+/**
+  * Created by nebiroz on 9/22/16.
+  */
+object Lesson1 {
+  def main(args: Array[String]): Unit = {
+    for (arg <- args)
+      println(arg + "")
+  }
+
+}
---
+++ src/com/nebiroz/scala/Step9.scala
@@ -0,0 +1,10 @@
+package com.nebiroz.scala
+
+/**
+  * Created by nebiroz on 9/22/16.
+  */
+class Step9 {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
